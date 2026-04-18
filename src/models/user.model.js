b7387e5c0e1409ee4eb23bba12291c18b974bcf6
niz-ag <nishchalag0307@gmail.com>
2026-04-18 13:30:52 +0530

    implemented hooks-methods for user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,8 @@
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 userSchema = new Schema(
   {
@@ -66,7 +69,49 @@ userSchema = new Schema(
       type: Date,
     },
   },
-  { timestamps: true, },
+  { timestamps: true },
 );
+
+//implemetation of pre-hook
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+//implemetation of methods
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    { _id: this._id, email: this.email, username: this.username },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+};
+
+userSchema.methods.generateTemporaryToken = function () {
+  const unHashedToken = crypto.randomBytes(24).toString("hex");
+
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+
+  const tokenExpiry = Date.now() + 10 * 60 * 1000; //10 minutes
+
+  return { unHashedToken, hashedToken, tokenExpiry };
+};
 
 export const user = mongoose.model("User", userSchema);
