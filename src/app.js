79359033bc38846ffe20be90d1bac6b8d9d58fba
niz-ag <nishchalag0307@gmail.com>
2026-05-12 05:56:28 +0530

    project routes

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
 //cors configurations
 app.use(
   cors({
@@ -22,7 +23,9 @@ app.use(
 
 import healthCheckRouter from "./routes/healthcheck.route.js";
 import authRouter from "./routes/auth.route.js";
+import projectRouter from "./routes/project.route.js";
 app.use("/api/v1/healthcheck", healthCheckRouter);
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/projects", projectRouter);
 
 export default app;
---
+++ src/routes/project.route.js
@@ -0,0 +1,64 @@
+import { Router } from "express";
+
+import {
+  getProjects,
+  getProjectsById,
+  createProjects,
+  updateProjects,
+  deleteProjects,
+  addMembersToProjects,
+  getProjectMembers,
+  updateMemberRole,
+  deleteMembersFromProject,
+} from "../controllers/project.controller.js";
+
+import {
+  addMembertoProjectValidator,
+  createProjectValidator,
+} from "../validators";
+
+import { validate } from "../middlewares/validator.middleware.js";
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { validateProjectPermission } from "../middlewares/project.middleware.js";
+import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
+
+const router = Router();
+
+router.use(verifyJWT); //middleware for protected routes
+
+router
+  .route("/")
+  .get(getProjects)
+  .post(createProjectValidator(), validate, createProjects);
+
+router
+  .route("/:projectId")
+  .get(validateProjectPermission(AvailableUserRole), getProjectsById)
+  .put(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    createProjectValidator(),
+    validate,
+    updateProjects,
+  )
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteProjects);
+
+router
+  .route("/:projectId/members/")
+  .get(getProjectMembers)
+  .post(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    addMembertoProjectValidator(),
+    validate,
+    addMembersToProjects,
+  );
+
+router
+  .route("/:projectId/members/:userId")
+  .put(validateProjectPermission([UserRolesEnum.ADMIN]), updateMemberRole)
+  .delete(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    deleteMembersFromProject,
+  );
+
+export default Router;
